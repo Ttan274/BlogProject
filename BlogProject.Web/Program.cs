131befb1359c7996c509a5fcdc34a426e78549fc
TanTalha <93757948+Ttan274@users.noreply.github.com>
2024-01-26 23:47:46 +0300

    Admin area created

--- BlogProject.Web/Program.cs
+++ BlogProject.Web/Program.cs
@@ -24,8 +24,14 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapAreaControllerRoute(
+        name : "Admin",
+        areaName: "Admin",
+        pattern: "Admin/{controller=Home}/{action=index}/{id?}"
+    );
+    endpoints.MapDefaultControllerRoute();
+});
 
 app.Run();
